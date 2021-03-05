e7f1dd666b322678af9342643e64011b70a1cf1b
b0006 <qsefthuken@gmail.com>
2021-03-05 12:34:08 +0700

    fea(project): init typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,18 @@ import type { AppProps /*, AppContext */ } from 'next/app';
 import Head from 'next/head';
 
 import 'src/styles/index.scss';
-import 'gridlex/src/gridlex.scss';
-
-import Layout from 'src/modules/layout/components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <Layout>
+    <div>
       <Head>
-        <title>Package.json viewer</title>
+        <title>Online shop</title>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
-    </Layout>
+    </div>
   );
 }
 
