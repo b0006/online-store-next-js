c5ef8b1ee664d36946b63d1f711371ed132ec1ea
b0006 <qsefthuken@gmail.com>
2021-03-25 19:32:26 +0700

    fea(header): menu mobile style

--- src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/index.tsx
+++ src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cn from 'classnames';
 
+import Container from 'src/modules/layout/components/Container';
 import SvgIcon from 'src/modules/common/components/SvgIcon';
 import { categoryMock } from 'src/mock';
 
@@ -25,30 +26,33 @@ const HeaderMenuMobile: React.FC<IProps> = ({ isShowedMenu, onClose }) => {
         [styles['menu-mobile_show']]: isShowedMenu,
       })}
     >
-      <div className={styles['menu-mobile__header']}>
-        <button type="button" onClick={onBackClick}>
-          <SvgIcon style={{ width: '20px' }} kind="chevron" />
+      <Container className={styles['menu-mobile__header']}>
+        <button className={styles['menu-mobile__back']} type="button" onClick={onBackClick}>
+          <SvgIcon className={styles['menu-mobile__icon']} kind="chevron" />
         </button>
         <div>{currentTitle}</div>
-      </div>
-      <ul>
+      </Container>
+      <ul className={styles['menu-mobile__list']}>
         {currentMenuList.map((menu) => {
           const hasChildren = menu.childList && menu.childList.length;
 
-          if (hasChildren) {
-            return (
-              <li key={menu.id}>
-                <span>{menu.title}</span>
-                <button key={menu.id} onClick={() => onCategoryChange(menu.id)}>
-                  <SvgIcon style={{ width: '20px' }} kind="chevron" />
-                </button>
-              </li>
-            );
-          }
-
           return (
             <li key={menu.id}>
-              <span>{menu.title}</span>
+              <Container className={styles['menu-mobile__item']}>
+                <div className={styles['menu-mobile__title']}>{menu.title}</div>
+                {hasChildren && (
+                  <button
+                    className={styles['menu-mobile__button']}
+                    key={menu.id}
+                    onClick={() => onCategoryChange(menu.id)}
+                  >
+                    <SvgIcon
+                      className={cn(styles['menu-mobile__icon'], styles['menu-mobile__icon_right'])}
+                      kind="chevron"
+                    />
+                  </button>
+                )}
+              </Container>
             </li>
           );
         })}
