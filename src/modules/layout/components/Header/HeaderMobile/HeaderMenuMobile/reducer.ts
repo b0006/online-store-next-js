f3444b480f653a0899d90726ca69c8ecb55545f4
b0006 <qsefthuken@gmail.com>
2021-04-19 00:54:07 +0700

    ref(header): mobile menu reducer

--- src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/reducer.ts
+++ src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/reducer.ts
@@ -18,12 +18,9 @@ export interface ICategoryItem {
 }
 
 export enum ACTIONS {
-  CHANGE_CATEGORY,
-}
-
-interface IAction {
-  type: ACTIONS;
-  payload?: Partial<IState>;
+  NEXT,
+  JUMP,
+  PREV,
 }
 
 export const initialState: IState = {
@@ -34,16 +31,124 @@ export const initialState: IState = {
   isRoot: true,
 };
 
-export function reducer(state: IState, action: IAction): IState {
+const getCategoryDataById = (
+  categoryList: ICategoryItem[],
+  categoryId?: number | null
+): {
+  title?: string;
+  list?: ICategoryItem[];
+} => {
+  if (categoryId === null) {
+    return {
+      title: 'Menu',
+      list: categoryList,
+    };
+  }
+
+  return categoryList.reduce((acc, category) => {
+    if (category.id !== categoryId && category.childList) {
+      return categoryId
+        ? { ...acc, ...getCategoryDataById(category.childList, categoryId) }
+        : getCategoryDataById(category.childList, categoryId);
+    }
+    return category.childList
+      ? {
+          ...acc,
+          title: category.title,
+          list: category.childList,
+        }
+      : acc;
+  }, {});
+};
+
+const addToHistory = <T = number>(list: T[], target: T): T[] => {
+  const cloneList = [...list];
+  cloneList.push(target);
+  return cloneList;
+};
+
+const removeFromDataList = (list: IBreadcrumb[], targetId: number | null): IBreadcrumb[] => {
+  if (typeof targetId !== 'number') {
+    return list;
+  }
+
+  const cloneList = [...list];
+
+  const findIndex = list.findIndex((item) => item.id === targetId);
+  if (findIndex > -1) {
+    cloneList.splice(findIndex, 1);
+  }
+
+  return cloneList;
+};
+
+const removeAfterCategoryId = (list: IBreadcrumb[], afterId: number): IBreadcrumb[] => {
+  const cloneList = [...list];
+  const findIndex = list.findIndex((item) => item.id === afterId);
+  if (findIndex > -1) {
+    cloneList.length = findIndex + 1;
+  }
+  return cloneList;
+};
+
+type Action =
+  | {
+      type: ACTIONS.NEXT;
+      payload: {
+        currentCategoryId: number;
+      };
+    }
+  | {
+      type: ACTIONS.PREV;
+    }
+  | {
+      type: ACTIONS.JUMP;
+      payload: {
+        currentCategoryId: number;
+      };
+    };
+
+export function reducer(state: IState, action: Action): IState {
   switch (action.type) {
-    case ACTIONS.CHANGE_CATEGORY:
+    case ACTIONS.NEXT: {
+      const categoryId = action.payload.currentCategoryId;
+      const { list, title } = getCategoryDataById(state.categoryList, categoryId);
+
       return {
         ...state,
-        currentCategoryId: action.payload?.currentCategoryId || null,
-        currentMenuList: action.payload?.currentMenuList || [],
-        breadcrumbList: action.payload?.breadcrumbList || [],
-        isRoot: !!action.payload?.isRoot,
+        currentCategoryId: categoryId,
+        currentMenuList: list || [],
+        breadcrumbList: addToHistory(state.breadcrumbList, { id: categoryId, title: title || '' }),
+        isRoot: false,
       };
+    }
+    case ACTIONS.PREV: {
+      const newBreadcrumbList = removeFromDataList(state.breadcrumbList, state.currentCategoryId);
+      const categoryId = newBreadcrumbList[newBreadcrumbList.length - 1]?.id || null;
+
+      const { list } = getCategoryDataById(state.categoryList, categoryId);
+
+      return {
+        ...state,
+        currentCategoryId: categoryId,
+        currentMenuList: list || [],
+        breadcrumbList: newBreadcrumbList,
+        isRoot: categoryId === null ? true : false,
+      };
+    }
+    case ACTIONS.JUMP: {
+      const categoryId = action.payload.currentCategoryId;
+      const { list } = getCategoryDataById(state.categoryList, categoryId);
+      const newBreadcrumbList = removeAfterCategoryId(state.breadcrumbList, categoryId);
+
+      return {
+        ...state,
+        currentCategoryId: action.payload.currentCategoryId,
+        currentMenuList: list || [],
+        breadcrumbList: newBreadcrumbList,
+        isRoot: newBreadcrumbList.length <= 0,
+      };
+    }
     default:
       return initialState;
   }
--- src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/useCategories.ts
+++ src/modules/layout/components/Header/HeaderMobile/HeaderMenuMobile/useCategories.ts
@@ -10,66 +10,6 @@ interface IReturn {
   onJumpCategory: (categoryId: number) => void;
 }
 
-const getCategoryDataById = (
-  categoryList: ICategoryItem[],
-  categoryId?: number | null
-): {
-  title?: string;
-  list?: ICategoryItem[];
-} => {
-  if (categoryId === null) {
-    return {
-      title: 'Menu',
-      list: categoryList,
-    };
-  }
-
-  return categoryList.reduce((acc, category) => {
-    if (category.id !== categoryId && category.childList) {
-      return categoryId
-        ? { ...acc, ...getCategoryDataById(category.childList, categoryId) }
-        : getCategoryDataById(category.childList, categoryId);
-    }
-    return category.childList
-      ? {
-          ...acc,
-          title: category.title,
-          list: category.childList,
-        }
-      : acc;
-  }, {});
-};
-
-const removeAfterCategoryId = (list: IBreadcrumb[], afterId: number): IBreadcrumb[] => {
-  const cloneList = [...list];
-  const findIndex = list.findIndex((item) => item.id === afterId);
-  if (findIndex > -1) {
-    cloneList.length = findIndex + 1;
-  }
-  return cloneList;
-};
-
-const removeFromDataList = (list: IBreadcrumb[], targetId: number | null): IBreadcrumb[] => {
-  if (typeof targetId !== 'number') {
-    return list;
-  }
-
-  const cloneList = [...list];
-
-  const findIndex = list.findIndex((item) => item.id === targetId);
-  if (findIndex > -1) {
-    cloneList.splice(findIndex, 1);
-  }
-
-  return cloneList;
-};
-
-const addToHistory = <T = number>(list: T[], target: T): T[] => {
-  const cloneList = [...list];
-  cloneList.push(target);
-  return cloneList;
-};
-
 const useCategories = (categoryList: ICategoryItem[]): IReturn => {
   const [state, dispatch] = useReducer(reducer, {
     ...initialState,
@@ -78,51 +18,22 @@ const useCategories = (categoryList: ICategoryItem[]): IReturn => {
   });
 
   const onBackHistory = (): void => {
-    const newBreadcrumbList = removeFromDataList(state.breadcrumbList, state.currentCategoryId);
-    const categoryId = newBreadcrumbList[newBreadcrumbList.length - 1]?.id || null;
-    const { list } = getCategoryDataById(state.categoryList, categoryId);
-
-    dispatch({
-      type: ACTIONS.CHANGE_CATEGORY,
-      payload: {
-        currentCategoryId: categoryId,
-        currentMenuList: list,
-        breadcrumbList: newBreadcrumbList,
-        isRoot: categoryId === null ? true : false,
-      },
-    });
+    dispatch({ type: ACTIONS.PREV });
   };
 
   const onCategoryChange = (categoryId: number): void => {
-    const { list, title } = getCategoryDataById(state.categoryList, categoryId);
-
     dispatch({
-      type: ACTIONS.CHANGE_CATEGORY,
-      payload: {
-        currentCategoryId: categoryId,
-        currentMenuList: list,
-        breadcrumbList: addToHistory(state.breadcrumbList, { id: categoryId, title: title || '' }),
-        isRoot: false,
-      },
+      type: ACTIONS.NEXT,
+      payload: { currentCategoryId: categoryId },
     });
   };
 
   const onJumpCategory = (categoryId: number): void => {
-    const { list } = getCategoryDataById(state.categoryList, categoryId);
-    const newBreadcrumbList = removeAfterCategoryId(state.breadcrumbList, categoryId);
-
     dispatch({
-      type: ACTIONS.CHANGE_CATEGORY,
-      payload: {
-        currentCategoryId: categoryId,
-        currentMenuList: list,
-        breadcrumbList: newBreadcrumbList,
-        isRoot: newBreadcrumbList.length <= 0,
-      },
+      type: ACTIONS.JUMP,
+      payload: { currentCategoryId: categoryId },
     });
   };
-
-  console.log(state);
 
   return {
     breadcrumbList: state.breadcrumbList,
